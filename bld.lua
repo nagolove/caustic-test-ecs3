482d3d1b586e8beeadaa4a0fd862cadcd4764a4e
nagolove <dekar.own@gmail.com>
2024-09-20 12:42:14 +0300

    module 'rlwr' disabled due linking errors

--- bld.lua
+++ bld.lua
@@ -1,11 +1,33 @@
+
 return {
    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       artifact = "ecs-test",
       main = "main.c",
       src = "src",
       not_dependencies = {
          "lfs",
          "resvg",
+         "rlwr",
       },
    },
 }
