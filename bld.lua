1a20759d0f6fc9a6cf02fad2b12ecad7f5395e96
nagolove <dekar.own@gmail.com>
2024-09-23 15:18:25 +0300

    Project dependency example(non working yet)

--- bld.lua
+++ bld.lua
@@ -1,24 +1,23 @@
 
 return {
    {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      proj_dependencies = {
+         {
+            disabled = false,
+            build = "@build_with_cmake",
+            description = "svg parsing library",
+            dir = "nanosvg",
+            includes = {
+               "nanosvg/src",
+            },
+            libdirs = { "nanosvg" },
+            links = { "nanosvg" },
+            links_internal = {},
+            name = "nanosvg",
+            url_action = "git",
+            url = "https://github.com/memononen/nanosvg.git",
+         },
+      },
 
 
       artifact = "ecs-test",
